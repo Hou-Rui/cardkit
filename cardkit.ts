332bc9ad8ce4e644ea42976e0467bea0bc7f51f9
Hou-Rui <13244639785@163.com>
2019-03-12 23:50:11 +0800

    init version

---
+++ cardkit.ts
@@ -0,0 +1,271 @@
+module cardkit {
+
+    export enum Suit {
+        Spades = 'S',
+        Hearts = 'H',
+        Clubs = 'C',
+        Diamonds = 'D'
+    }
+
+    export const SuitFromString: { [key: string]: Suit } = {
+        'S': Suit.Spades,
+        'H': Suit.Hearts,
+        'C': Suit.Clubs,
+        'D': Suit.Diamonds
+    }
+
+    export const SuitFromNumber: Suit[] = [
+        Suit.Spades, Suit.Hearts, Suit.Clubs, Suit.Diamonds
+    ]
+
+    export function suitConflict(a: Suit, b: Suit) {
+        if (a === Suit.Spades || a === Suit.Clubs) {
+            return b === Suit.Hearts || b === Suit.Diamonds
+        }
+        else {
+            return b === Suit.Spades || b === Suit.Clubs
+        }
+    }
+
+    class Wrapper {
+        public element = document.createElement('div')
+
+        constructor(public scene: HTMLElement) {
+            this.style().position = 'absolute'
+            this.style().width = '71px'
+            this.style().height = '96px'
+            this.style().borderRadius = '4px'
+            scene.style.zIndex = '-1'
+            scene.appendChild(this.element)
+        }
+        style() {
+            return this.element.style
+        }
+        left() {
+            return Number(this.style().left!.split('px')[0])
+        }
+        top() {
+            return Number(this.style().top!.split('px')[0])
+        }
+        width() {
+            return Number(this.style().width!.split('px')[0])
+        }
+        height() {
+            return Number(this.style().height!.split('px')[0])
+        }
+        containsPoint(left: number, top: number) {
+            return (left > this.left() && left < this.left() + this.width()
+                && top > this.top() && top < this.top() + this.height())
+        }
+        move(left: number, top: number) {
+            this.style().left = `${left}px`
+            this.style().top = `${top}px`
+        }
+        onClick(event: MouseEvent): void {
+            // 重载此方法
+        }
+        onDoubleClick(event: MouseEvent): void {
+            // 重载此方法
+        }
+        movable(event: MouseEvent): boolean {
+            // 重载此方法
+            return true
+        }
+    }
+
+    class MovingEventHandler {
+        private movingCard: Card | null = null
+        private deltaX = 0
+        private deltaY = 0
+        private static _instance: MovingEventHandler | null = null
+        private static readonly TopZIndex = 10000
+        private constructor() {
+            document.onmousemove = (event) => {
+                if (this.movingCard !== null) {
+                    let x = event.clientX - this.deltaX
+                    let y = event.clientY - this.deltaY
+                    this.movingCard.moveWithNext(x, y)
+                }
+            }
+        }
+        static instance() {
+            if (MovingEventHandler._instance === null) {
+                return MovingEventHandler._instance = new MovingEventHandler()
+            }
+            return MovingEventHandler._instance
+        }
+        moveBegin(event: MouseEvent, card: Card) {
+            this.movingCard = card
+            this.deltaX = event.clientX - card.left()
+            this.deltaY = event.clientY - card.top()
+            card.style().zIndex = `${MovingEventHandler.TopZIndex}`
+            let nextCards = card.next()
+            for (let index = 0; index < nextCards.length; index++) {
+                let newZIndex = MovingEventHandler.TopZIndex + index + 1
+                nextCards[index].style().zIndex = `${newZIndex}`
+            }
+        }
+        moveEnd(event: MouseEvent) {
+            let deck = Deck.puttingCardAt(this.movingCard!, event.clientX, event.clientY)
+            if (deck === null || !deck.acceptCard(this.movingCard!)) {
+                this.movingCard!.deck.adjustCardPosition()
+                this.movingCard = null
+                return
+            }
+            let nextCards = this.movingCard!.next()
+            deck.addCard(this.movingCard!)
+            for (let card of nextCards) {
+                deck.addCard(card)
+            }
+            this.movingCard = null
+        }
+    }
+
+    export class Deck extends Wrapper {
+        public cards = new Array<Card>()
+        public faceDownDelta = 2
+        public faceUpDelta = 15
+        public static pool = new Array<Deck>()
+        constructor(scene: HTMLElement, left: number = 0, top: number = 0) {
+            super(scene)
+            this.initStyle(left, top)
+            Deck.pool.push(this)
+        }
+        private initStyle(left: number, top: number) {
+            this.style().left = `${left}px`
+            this.style().top = `${top}px`
+            this.style().background = 'rgba(0, 0, 0, 0.2)'
+            this.style().zIndex = `0`
+        }
+        public adjustCardPosition() {
+            let left = Number(this.style().left!.split('px')[0])
+            let top = Number(this.style().top!.split('px')[0])
+            for (let index = 0; index < this.cards.length; index++) {
+                let card = this.cards[index]
+                card.rank = index
+                card.style().zIndex = `${index + 1}`
+                card.move(left, top)
+                top += (card.faceUp() ? this.faceUpDelta : this.faceDownDelta)
+            }
+        }
+        static puttingCardAt(movingCard: Card, left: number, top: number) {
+            for (let deck of Deck.pool) {
+                if (deck === movingCard.deck) {
+                    continue
+                }
+                let card = deck.topCard()
+                if (card === null) {
+                    if (deck.containsPoint(left, top)) {
+                        return deck
+                    }
+                }
+                else if (card.containsPoint(left, top)) {
+                    return deck
+                }
+            }
+            return null
+        }
+        addCard(card: Card) {
+            card.deck.removeCard(card)
+            this.cards.push(card)
+            card.deck = this
+            this.adjustCardPosition()
+        }
+        topCard() {
+            if (this.cards.length === 0) {
+                return null
+            }
+            return this.cards[this.cards.length - 1]
+        }
+        removeCard(card: Card) {
+            this.cards = this.cards.filter((x) => x !== card)
+        }
+        shuffle() {
+            this.cards.sort((_x, _y) => Math.random() > 0.5 ? -1 : 1)
+            this.adjustCardPosition()
+        }
+        acceptCard(card: Card): boolean {
+            // 重载此方法
+            return true
+        }
+        onCardClick(card: Card, event: MouseEvent): void {
+            // 重载此方法
+        }
+        onCardDoubleClick(card: Card, event: MouseEvent): void {
+            // 重载此方法
+        }
+    }
+
+    export class Card extends Wrapper {
+        public rank = -1
+        public name: string
+        private _faceUp = true
+        constructor(public deck: Deck, public suit: Suit, public point: number) {
+            super(deck.scene)
+            // 初始化卡牌
+            this.name = `${this.suit}${this.point}`
+            this.setFaceUp(false)
+            deck.addCard(this)
+            // 初始化事件
+            this.initEvents()
+        }
+        private initEvents() {
+            let handler = MovingEventHandler.instance()
+            this.element.onmousedown = (event) => {
+                handler.moveBegin(event, this)
+                this.onMoveBegin(event)
+            }
+            this.element.onmouseup = (event) => {
+                handler.moveEnd(event)
+                this.onMoveEnd(event)
+            }
+            this.element.onclick = (event) => {
+                this.onClick(event)
+                this.deck.onCardClick(this, event)
+            }
+            this.element.ondblclick = (event) => {
+                this.onDoubleClick(event)
+                this.deck.onCardDoubleClick(this, event)
+            }
+        }
+        static fromName(deck: Deck, name: string) {
+            let suit = SuitFromString[name[0]]
+            let point = Number(name.slice(1))
+            return new this(deck, suit, point)
+        }
+        next(): Card[] {
+            let cards = this.deck.cards
+            return cards.slice(this.rank + 1)
+        }
+        faceUp(): boolean {
+            return this._faceUp
+        }
+        setFaceUp(up: boolean) {
+            this._faceUp = up
+            if (up) {
+                this.style().backgroundImage = `url("images/${this.name}.bmp")`
+            }
+            else {
+                this.style().backgroundImage = `url("images/back.bmp")`
+            }
+        }
+        moveWithNext(left: number, top: number) {
+            let startX = this.left(), startY = this.top()
+            this.move(left, top)
+            let nextCards = this.next()
+            for (let card of nextCards) {
+                let deltaX = left - startX,  deltaY = top - startY
+                let newX = card.left() + deltaX
+                let newY = card.top() + deltaY
+                card.move(newX, newY)
+            }
+        }
+        onMoveBegin(event: MouseEvent): void {
+            // 重载此方法
+        }
+        onMoveEnd(event: MouseEvent): void {
+            // 重载此方法
+        }
+    }
+
+}
---
+++ solitaire.ts
@@ -0,0 +1,149 @@
+///<reference path="cardkit.ts" />
+
+let game: Solitaire
+
+class GameCard extends cardkit.Card {
+    constructor(deck: cardkit.Deck, suit: cardkit.Suit, point: number,
+        private targets: TargetDeck[]) {
+        super(deck, suit, point)
+    }
+    onClick(_event: MouseEvent) {
+        if (!this.faceUp()) {
+            this.setFaceUp(true)
+        }
+    }
+    movable(_event: MouseEvent) {
+        return this.faceUp()
+    }
+    onDoubleClick(_event: MouseEvent) {
+        for (let deck of this.targets) {
+            if (deck.acceptCard(this)) {
+                deck.addCard(this)
+            }
+        }
+    }
+    onMoveEnd(_event: MouseEvent) {
+        game.checkGameOver()
+    }
+}
+
+class ServingDeck extends cardkit.Deck {
+    public replayCount = 0
+    constructor(scene: HTMLElement, left: number, top: number,
+            private placingDeck: PlacingDeck) {
+        super(scene, left, top)
+        this.faceUpDelta = this.faceDownDelta = 0
+        placingDeck.move(left + 100, top)
+    }
+    acceptCard(_card: cardkit.Card) {
+        return false
+    }
+    onClick(_event: MouseEvent) {
+        this.replayCount++
+        for (let card of this.placingDeck.cards) {
+            this.addCard(card)
+        }
+    }
+    onCardClick(card: GameCard, _event: MouseEvent) {
+        this.placingDeck.addCard(card)
+    }
+}
+
+class NormalDeck extends cardkit.Deck {
+    acceptCard(card: cardkit.Card) {
+        let topCard = this.topCard()
+        if (topCard === null) {
+            return card.point === 13
+        }
+        return card.point === topCard.point - 1
+            && cardkit.suitConflict(card.suit, topCard.suit)
+    }
+}
+
+class TargetDeck extends cardkit.Deck {
+    public replayCount = 0
+    constructor(scene: HTMLElement, private suit: cardkit.Suit) {
+        super(scene)
+        this.faceUpDelta = this.faceDownDelta = 0
+    }
+    acceptCard(card: cardkit.Card) {
+        let topCard = this.topCard()
+        if (topCard === null) {
+            return card.point === 1
+                && card.suit === this.suit
+        }
+        return card.point === topCard.point + 1
+            && card.suit === topCard.suit
+    }
+}
+
+class PlacingDeck extends cardkit.Deck {
+    constructor(scene: HTMLElement, left = 0, top = 0) {
+        super(scene, left, top)
+        this.faceUpDelta = this.faceDownDelta = 0
+    }
+    acceptCard(_card: cardkit.Card) {
+        return false
+    }
+}
+
+class Solitaire {
+    private placingDeck = new PlacingDeck(this.scene)
+    private servingDeck = new ServingDeck(this.scene, 50, 50, this.placingDeck)
+    private normalDecks = new Array<NormalDeck>()
+    private targetDecks = new Array<TargetDeck>()
+    static Height = 700
+    static Width = 1000
+    constructor(private scene: HTMLElement) {
+        this.initSceneStyle()
+        this.initServingDeck()
+        this.initNormalDecks()
+        this.initTargetDecks()
+    }
+    private initSceneStyle() {
+        this.scene.style.background = 'rgb(0, 128, 0)'
+        this.scene.style.height = `${Solitaire.Height}px`
+        this.scene.style.width = `${Solitaire.Width}px`
+    }
+    private initServingDeck() {
+        for (let suit of cardkit.SuitFromNumber) {
+            for (let point = 1; point <= 13; point++) {
+                new GameCard(this.servingDeck, suit, point, this.targetDecks)
+            }
+        }
+        this.servingDeck.shuffle()
+    }
+    private initNormalDecks() {
+        const singleSpace = Solitaire.Width / 7
+        for (let index = 0; index < 7; index++) {
+            this.normalDecks[index] = new NormalDeck(this.scene)
+            let deck = this.normalDecks[index]
+            const mid = singleSpace * (index + 0.5)
+            const left = Math.floor(mid - deck.width() / 2)
+            deck.move(left, 200)
+            for (let cnt = 0; cnt < index + 1; cnt++) {
+                deck.addCard(this.servingDeck.topCard()!)
+            }
+            deck.topCard()!.setFaceUp(true)
+        }
+    }
+    private initTargetDecks() {
+        const singleSpace = (Solitaire.Width - 400) / 4
+        for (let index = 0; index < 4; index++) {
+            let suit = cardkit.SuitFromNumber[index]
+            this.targetDecks[index] = new TargetDeck(this.scene, suit)
+            let deck = this.targetDecks[index]
+            const mid = singleSpace * (index + 0.5)
+            const left = Math.floor(Solitaire.Width - mid - deck.width() / 2)
+            deck.move(left, 50)
+        }
+    }
+    public checkGameOver() {
+        if (this.targetDecks.every(deck => deck.cards.length === 13)) {
+            alert('游戏完成！')
+        }
+    }
+}
+
+let scene = document.getElementById('game-scene')
+game = new Solitaire(scene!)
